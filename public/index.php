d6b87d1f951eed88ea5c0ff0a9d1ff7b4c177d70
KIRIN_TING <kanchitpol29092003@gmail.com>
2026-02-07 13:12:38 +0700

    cse

--- public/index.php
+++ public/index.php
@@ -1,14 +1,5 @@
 <?php
 
-header('Access-Control-Allow-Origin: https://internproject-front.vercel.app');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
-
 define('LARAVEL_START', microtime(true));
 
 // โหลด Autoloader (ใส่ / เพิ่มเข้าไปหลัง __DIR__)
